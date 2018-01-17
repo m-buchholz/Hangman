8dfd8bfc4d068b72b7635081f0f249cbc304126e
Maximilian Buchholz <maximilianbuchholz@web.de>
2018-01-17 16:32:41 +0100

    -Titelbild -END-Screen bearbeitet und Buttons ergaenzt

--- app/src/main/java/de/tu_dresden/hangman/MultiplayerWord.java
+++ app/src/main/java/de/tu_dresden/hangman/MultiplayerWord.java
@@ -3,6 +3,7 @@ package de.tu_dresden.hangman;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
