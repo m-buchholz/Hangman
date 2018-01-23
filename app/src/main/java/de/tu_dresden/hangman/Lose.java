3cad7d4624069ff3ecc51194dc2e7988401ad963
RicoAdler <enrico-adler@web.de>
2018-01-23 12:42:05 +0100

    Share Button

--- app/src/main/java/de/tu_dresden/hangman/Lose.java
+++ app/src/main/java/de/tu_dresden/hangman/Lose.java
@@ -4,21 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Lose extends AppCompatActivity {
 
-    String playerName;
+    String playerName, word;
     TextView notice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lose);
-
+        word = getIntent().getExtras().getString("word");
         playerName = getIntent().getExtras().getString("playerName");
         notice = (TextView) findViewById(R.id.lose_notice);
         notice.setText("Du hast leider verloren, "+playerName+".");
+
+        Button button = (Button) findViewById(R.id.share);          //Share Button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("text/plain");
+                share.putExtra(Intent.EXTRA_SUBJECT,"Hangman");
+                String counter = "Verloren nach: ";
+                String platzhalter = "                                              ";
+                String result = " wurde erhängt!";
+                String player = playerName;
+                share.putExtra(Intent.EXTRA_TEXT,"" + player + result + platzhalter + counter /*+ Versuchen*/+ "Versuchen." + platzhalter + "Das Wort war: " + word );
+                startActivity(Intent.createChooser(share, "Zeige es deinen Freunden"));
+            }
+        });
     }
 
     public void openSingleplayer(View view){
--- app/src/main/java/de/tu_dresden/hangman/Win.java
+++ app/src/main/java/de/tu_dresden/hangman/Win.java
@@ -4,11 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Win extends AppCompatActivity {
 
-    String playerName;
+    String playerName, word;
     TextView notice;
 
     @Override
@@ -16,9 +17,26 @@ public class Win extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win);
 
+        word = getIntent().getExtras().getString("word");
         playerName = getIntent().getExtras().getString("playerName"); //Spielernamen importieren
         notice = (TextView) findViewById(R.id.win_notice);
         notice.setText("Herzlichen Glückwunsch "+playerName+", du hast gewonnen!");
+
+        Button button = (Button) findViewById(R.id.share);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("text/plain");
+                share.putExtra(Intent.EXTRA_SUBJECT,"Hangman");
+                String counter = "Geschafft nach: ";
+                String platzhalter = "                                              ";
+                String result = " hat überlebt!";
+                String player = playerName;
+                share.putExtra(Intent.EXTRA_TEXT,"" + player + result + platzhalter + counter /*+ Versuchen*/+ "Versuchen." + platzhalter + "Das Wort war: " + word );
+                startActivity(Intent.createChooser(share, "Zeige es deinen Freunden"));
+            }
+        });
     }
 
     public void openSingleplayer(View view){
