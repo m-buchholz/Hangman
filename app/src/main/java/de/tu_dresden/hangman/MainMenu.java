ae7623a8a9d8dbd96b4460d6dec810cb5f6cd193
FarmerForLive <nico1996bauer@gmail.com>
2018-01-23 11:23:59 +0100

    Spieler-Objekt für Scoreboard erstellt.

--- app/src/main/java/de/tu_dresden/hangman/MainMenu.java
+++ app/src/main/java/de/tu_dresden/hangman/MainMenu.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import java.io.*;
 
 public class MainMenu extends AppCompatActivity {
 
