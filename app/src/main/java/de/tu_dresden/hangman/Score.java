5197e0f90ef2efbd00587481e9cf6765aeb1541e
Maximilian <maximilianbuchholz@web.de>
2018-01-30 17:21:06 +0100

    -Scoreboard Ausgabe bearbeitet

--- app/src/main/java/de/tu_dresden/hangman/Score.java
+++ app/src/main/java/de/tu_dresden/hangman/Score.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -47,16 +48,16 @@ import android.widget.ListView;
 
 public class Score extends AppCompatActivity {
 
-    public static final String PLATZ_10 = "Platz 10: ";
-    public static final String PLATZ_9 = "Platz 9: ";
-    public static final String PLATZ_8 = "Platz 8: ";
-    public static final String PLATZ_7 = "Platz 7: ";
-    public static final String PLATZ_6 = "Platz 6: ";
-    public static final String PLATZ_5 = "Platz 5: ";
-    public static final String PLATZ_4 = "Platz 4: ";
-    public static final String PLATZ_3 = "Platz 3: ";
-    public static final String PLATZ_2 = "Platz 2: ";
-    public static final String PLATZ_1 = "Platz 1: ";
+    public static final String PLATZ_10 ="PLATZ 10: ";
+    public static final String PLATZ_9 = "PLATZ 9: ";
+    public static final String PLATZ_8 = "PLATZ 8: ";
+    public static final String PLATZ_7 = "PLATZ 7: ";
+    public static final String PLATZ_6 = "PLATZ 6: ";
+    public static final String PLATZ_5 = "PLATZ 5: ";
+    public static final String PLATZ_4 = "PLATZ 4: ";
+    public static final String PLATZ_3 = "PLATZ 3: ";
+    public static final String PLATZ_2 = "PLATZ 2: ";
+    public static final String PLATZ_1 = "PLATZ 1: ";
     ListView Scoreboard;
 
     String scorePlayerString, scoreWordsString, scoreFalseWordsString;
@@ -79,16 +80,16 @@ public class Score extends AppCompatActivity {
         String[] scoreFalseWords = scoreFalseWordsString.split(":");
 
         String[] scorearray = {
-                PLATZ_1 + scorePlayer[0] + " mit " + scoreWords[0] + " Richtigen und " + scoreFalseWords[0] + " Falschen",
-                PLATZ_2 + scorePlayer[1] + " mit " + scoreWords[1] + " Richtigen und " + scoreFalseWords[1] + " Falschen",
-                PLATZ_3 + scorePlayer[2] + " mit " + scoreWords[2] + " Richtigen und " + scoreFalseWords[2] + " Falschen",
-                PLATZ_4 + scorePlayer[3] + " mit " + scoreWords[3] + " Richtigen und " + scoreFalseWords[3] + " Falschen",
-                PLATZ_5 + scorePlayer[4] + " mit " + scoreWords[4] + " Richtigen und " + scoreFalseWords[4] + " Falschen",
-                PLATZ_6 + scorePlayer[5] + " mit " + scoreWords[5] + " Richtigen und " + scoreFalseWords[5] + " Falschen",
-                PLATZ_7 + scorePlayer[6] + " mit " + scoreWords[6] + " Richtigen und " + scoreFalseWords[6] + " Falschen",
-                PLATZ_8 + scorePlayer[7] + " mit " + scoreWords[7] + " Richtigen und " + scoreFalseWords[7] + " Falschen",
-                PLATZ_9 + scorePlayer[8] + " mit " + scoreWords[8] + " Richtigen und " + scoreFalseWords[8] + " Falschen",
-                PLATZ_10 + scorePlayer[9] + " mit " + scoreWords[9] + " Richtigen und " + scoreFalseWords[9] + " Falschen",
+                PLATZ_1 + scorePlayer[0] + " - " + scoreWords[0] + " Richtige und " + scoreFalseWords[0] + " Falsche",
+                PLATZ_2 + scorePlayer[1] + " - " + scoreWords[1] + " Richtige und " + scoreFalseWords[1] + " Falsche",
+                PLATZ_3 + scorePlayer[2] + " - " + scoreWords[2] + " Richtige und " + scoreFalseWords[2] + " Falsche",
+                PLATZ_4 + scorePlayer[3] + " - " + scoreWords[3] + " Richtige und " + scoreFalseWords[3] + " Falsche",
+                PLATZ_5 + scorePlayer[4] + " - " + scoreWords[4] + " Richtige und " + scoreFalseWords[4] + " Falsche",
+                PLATZ_6 + scorePlayer[5] + " - " + scoreWords[5] + " Richtige und " + scoreFalseWords[5] + " Falsche",
+                PLATZ_7 + scorePlayer[6] + " - " + scoreWords[6] + " Richtige und " + scoreFalseWords[6] + " Falsche",
+                PLATZ_8 + scorePlayer[7] + " - " + scoreWords[7] + " Richtige und " + scoreFalseWords[7] + " Falsche",
+                PLATZ_9 + scorePlayer[8] + " - " + scoreWords[8] + " Richtige und " + scoreFalseWords[8] + " Falsche",
+                PLATZ_10 + scorePlayer[9] + " - " + scoreWords[9] + " Richtige und " + scoreFalseWords[9] + " Falsche",
         };
 
         Scoreboard = findViewById(R.id.Scoreboard);
